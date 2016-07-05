c28b8407ec39b5664803535ac983f667bbf240dd
chenshixin <shixin.chen@huaban.com>
2016-07-05 11:52:31 +0800

    Add icons to sample

--- sample/src/main/java/com/chenshixin/bottomnavigation/sample/MainActivity.java
+++ sample/src/main/java/com/chenshixin/bottomnavigation/sample/MainActivity.java
@@ -15,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigation bottomNavigationBar = (BottomNavigation) findViewById(R.id.bottomNavigationBar);
-        bottomNavigationBar.addItem(new BottomNavigationItem("001", R.drawable.icon111, R.drawable.icon222));
-        bottomNavigationBar.addItem(new BottomNavigationItem("002", R.drawable.icon111, R.drawable.icon222));
-        bottomNavigationBar.addItem(new BottomNavigationItem("003", R.drawable.icon111, R.drawable.icon222));
-        bottomNavigationBar.addItem(new BottomNavigationItem("004", R.drawable.icon111, R.drawable.icon222));
+        bottomNavigationBar.addItem(new BottomNavigationItem("Book", R.drawable.ic_account_balance_wallet_black_24dp, R.drawable.ic_add_shopping_cart_black_24dp));
+        bottomNavigationBar.addItem(new BottomNavigationItem("Fav", R.drawable.ic_add_shopping_cart_black_24dp, R.drawable.ic_alarm_black_24dp));
+        bottomNavigationBar.addItem(new BottomNavigationItem("White", R.drawable.ic_alarm_black_24dp, R.drawable.ic_android_black_24dp));
+        bottomNavigationBar.addItem(new BottomNavigationItem("Link", R.drawable.ic_android_black_24dp, R.drawable.ic_account_balance_wallet_black_24dp));
+        bottomNavigationBar.addItem(new BottomNavigationItem("Fav", R.drawable.ic_add_shopping_cart_black_24dp, R.drawable.ic_alarm_black_24dp));
         bottomNavigationBar.setTitleColorActive(Color.BLUE);
         bottomNavigationBar.setTitleColorInactive(Color.RED);
         bottomNavigationBar.setOnTabSelectedListener(new BottomNavigation.OnTabSelectedListener() {
@@ -32,17 +33,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabSelected(int position) {
-                Toast.makeText(MainActivity.this, position + " selected", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, position + " selected", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onTabUnselected(int position) {
-                Toast.makeText(MainActivity.this, position + " onTabUnselected", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, position + " onTabUnselected", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onTabReselected(int position) {
-                Toast.makeText(MainActivity.this, position + " onTabReselected", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, position + " 需要滚动到顶部", Toast.LENGTH_SHORT).show();
             }
         });
         bottomNavigationBar.initialise();
